044134eae815e84222fff95c1ce4dbf2c02313a0
Nhan121 <60571509+Nhan121@users.noreply.github.com>
2021-08-31 21:37:43 +0700

    Add files via upload

---
+++ model/train_classifier.py
@@ -0,0 +1,91 @@
+import sys
+import pandas as pd
+from sqlalchemy.engine import create_engine
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+import nltk
+nltk.download(['punkt', 'wordnet'])
+
+def load_data(database_filepath):
+    
+    engine = create_engine('sqlite:///InsertDatabaseName.db')
+    df = pd.read_sql_table(database_filepath, engine)
+    X = df['message'].values
+    y = df.iloc[:, 4:].values
+    category_names = list(df.iloc[:, 4:].columns)
+    
+    return X, y, category_names 
+
+def tokenize(text):
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+def build_model():
+    pipeline = Pipeline([
+                    ('vect', CountVectorizer(tokenizer=tokenize)),
+                    ('tfidf', TfidfTransformer()),
+                    ('clf', MultiOutputClassifier(RandomForestClassifier()) )
+                ])
+    return pipeline
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    y_pred = model.predict(X_test)
+
+    from sklearn.metrics import precision_score, recall_score, accuracy_score
+    
+    for k in range(len(category_names)):
+        print('Number of category ', k, '\t name: ', category_names[k], '.\n')
+        print('\t Accuracy = ', (y_pred[:, k] == Y_test[:,k]).mean(),
+              '\t % Precision: \t', precision_score(Y_test[:,k], y_pred[:,k]),
+              '\t % Recall : \t', recall_score(Y_test[:,k], y_pred[:,k])
+             )
+
+def save_model(model, model_filepath):
+    import pickle
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
